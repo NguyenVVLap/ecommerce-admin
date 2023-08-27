e05f40a7253560f8dc453e5d12fd5c0cdbaade62
NguyenVVLap <nlap741@gmail.com>
2023-08-27 18:56:39 +0700

    Clerk Authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+function AuthLayout({children} : {children : React.ReactNode}) {
+    return ( <div className="flex justify-center items-center h-full w-full">{children}</div> );
+}
+
+export default AuthLayout;
